d85600d196379575adc1f08288fe7cedc8501722
Qinghang Zhang <Qzhang9@mail.greenriver.edu>
2020-04-20 18:34:41 -0400

    add index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>pp2</title>
+</head>
+<body>
+<h1>Pairprogram 2 </h1>
+<?php
+    echo "“PHP Array Practice";
+?>
+</body>
+</html>
